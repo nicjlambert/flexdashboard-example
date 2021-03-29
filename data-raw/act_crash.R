f3d3a85791cc6827f4003560752b697ca644abb3
MaguireMaName <lambni85@outlook.com>
2021-03-29 09:30:43 +0000

    added filter checkbox

--- data-raw/act_crash.R
+++ data-raw/act_crash.R
@@ -1,7 +1,8 @@
 ## code to prepare `act_crash` dataset goes here
 
 #-------------------------------------------------------------------------------
-# ACT crash dara sourced from https://www.data.act.gov.au/Transport/ACT-Road-Crash-Data/6jn4-m8rx
+### ACT crash data sourced from ################################################
+### https://www.data.act.gov.au/Transport/ACT-Road-Crash-Data/6jn4-m8rx ########
 
 library(here)
 library(janitor)
@@ -9,15 +10,17 @@ library(sf)
 library(rgeos)
 
 #-------------------------------------------------------------------------------
-# point data
-point <- read.csv("data-raw/ACT_Road_Crash_Data.csv") %>%
+### point data #################################################################
+
+point <- read.csv(here::here("data-raw/ACT_Road_Crash_Data.csv")) %>%
   clean_names() %>%
   mutate(crash_hour = as.integer(substr(crash_time, 1, 2)))
 glimpse(point)
 
 #-------------------------------------------------------------------------------
-# polygon data (shapefile)
-shp <- st_read("data-raw/SA2_2016_AUST.shp") %>%
+### polygon data (shapefile) ###################################################
+
+shp <- st_read(here::here("data-raw/SA2_2016_AUST.shp")) %>%
   clean_names() %>%
   filter(ste_name16 == "Australian Capital Territory") %>%
   st_transform(4283)
@@ -29,7 +32,8 @@ shp$cent_lat <- st_coordinates(st_centroid(shp))[,2] # add latitude to sf
 st_crs(shp)
 
 #-------------------------------------------------------------------------------
-# join point and polygon data
+### join point and polygon data ################################################
+
 act_crash <- point %>%
   filter(!is.na(latitude),!is.na(longitude)) %>%
   st_as_sf(coords = c("longitude", "latitude"), crs = 4283) %>%
@@ -47,12 +51,14 @@ count <- act_crash %>%
 act_crashes_sa2 <- left_join(shp, count, by = c("sa2_name16" = "sa2_name16")) %>%
   mutate(crashes_sq_km = round(crashes / areasqkm16, 2),
          decile = ntile(crashes_sq_km, 10),
-         color = ifelse(decile == 1, "blue", ifelse(decile == 10, "red", "black")))
+         color = ifelse(decile == 1, "blue", ifelse(decile == 10, "red", "black")),
+         bins = ifelse(decile == 1 ,"Area in bottom 10% for accidents", ifelse(decile == 10, "Area in top 10% for accidents", NA)))
   #filter(!is.na(crashes)) %>%
 glimpse(act_crashes_sa2)
 
 #-------------------------------------------------------------------------------
-# save data
+### save data ##################################################################
+
 usethis::use_data(act_crashes_sa2, overwrite = TRUE)
 
 rm(point, shp, act_crash, count)
