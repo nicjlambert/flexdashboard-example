13864ad76443811c1b6e98772e3b606dbe10492e
nicjlambert <lambni85@outlook.com>
2022-12-21 23:26:09 +1100

    act_crash.R works e2e

--- data-raw/act_crash.R
+++ data-raw/act_crash.R
@@ -1,12 +1,14 @@
 #! /usr/bin/R
 
-# This script downloads and processes crash data from the Australian Capital Territory (ACT).
+# This script downloads and processes crash data from the ACT.
 
 # Set the time zone to "Australia/Sydney"
-Sys.setenv("TZ"="Australia/Sydney")
+Sys.setenv("TZ" = "Australia/Sydney")
 
 # Define a list of packages to check
 required_packages <- c("janitor", "rgeos", "sf", "dplyr", "stringr")
+
+repos <- "https://cran.csiro.au"
 
 # Check if the required packages are installed and install them if necessary
 install_required_packages <- function(pkgs) {
@@ -14,7 +16,10 @@ install_required_packages <- function(pkgs) {
     # Check if the package is installed
     if (!require(pkg, character.only = TRUE)) {
       # Install the package with dependencies
-      install.packages(pkg, dependencies = TRUE, repos = "https://cran.csiro.au")
+      install.packages(pkg,
+      dependencies = TRUE,
+      repos = repos
+)
     }
     # Load the package into the current R session
     library(pkg, character.only = TRUE)
@@ -31,17 +36,17 @@ csv_url <- "https://www.data.act.gov.au/api/views/6jn4-m8rx/rows.csv?accessType=
 download_and_read_csv <- function(url) {
   # Create a temporary file
   temp_file <- tempfile()
-  
+
   # Download the CSV file to the temporary file
   download.file(url, temp_file)
-  
+
   # Read the CSV file into an R data frame
   df <- read.csv(temp_file)
   print(head(df))
-  
+
   # Delete the temporary file
   unlink(temp_file)
-  
+
   # Return the data frame
   return(df)
 }
@@ -54,15 +59,15 @@ process_data <- function(df) {
   # Clean and transform the data as needed
   cleaned_df <- df %>%
     clean_names() %>%
-    mutate(year = substr(crash_date,7,10))
-  
+    mutate(year = substr(crash_date, 7, 10))
+
   # Return the cleaned data frame
   return(cleaned_df)
 }
 
 # Process the data
 processed_data <- process_data(point_data)
-print(head(processed_data))
+
 # Define a function to create the `act_crash` dataset
 create_act_crash_dataset <- function(df) {
    # Create the `act_crash` dataset from the processed data
@@ -70,16 +75,13 @@ create_act_crash_dataset <- function(df) {
    select(year, crash_id, latitude, longitude, crash_severity) %>%
     group_by(year, crash_id, latitude, longitude, crash_severity) %>%
     summarize(count = n())
-  
+
   # Return the `act_crash` dataset
   return(act_crash)
 }
 
 # Create the `act_crash` dataset
 act_crash <- create_act_crash_dataset(processed_data)
-
-# Print the first few rows of the `act_crash` dataset
-print(head(act_crash))
 
 # Read features from shape file
 sa2_shp <- st_read("SA2_2016_AUST.shp") %>%
@@ -89,8 +91,8 @@ sa2_shp <- st_read("SA2_2016_AUST.shp") %>%
 
 # Find centroid of each county and add columns to data frame
 sa2_shp <- sa2_shp %>%
-  mutate(cent_lng = st_coordinates(st_centroid(sa2_shp))[,1],
-         cent_lat = st_coordinates(st_centroid(sa2_shp))[,2])
+  mutate(cent_lng = st_coordinates(st_centroid(sa2_shp))[, 1],
+         cent_lat = st_coordinates(st_centroid(sa2_shp))[, 2])
 
 # Check the coordinate reference system (CRS) of object
 st_crs(sa2_shp)
@@ -98,7 +100,7 @@ st_crs(sa2_shp)
 # Join point and polygon data 
 
 act_crash <- act_crash %>%
-  filter(!is.na(latitude),!is.na(longitude)) %>%
+  filter(!is.na(latitude), !is.na(longitude)) %>%
   st_as_sf(coords = c("longitude", "latitude"), crs = 4283) %>%
   st_join(sa2_shp["sa2_name16"])
 
@@ -106,15 +108,17 @@ act_crash <- act_crash %>%
 count <- act_crash %>%
   as_tibble() %>%
   group_by(sa2_name16) %>%
-  summarise(
-    crashes = n_distinct(crash_id))
+  summarise(crashes = n_distinct(crash_id))
 
-# join  count with sa2 in ACT, calc crash density
-act_crashes_sa2 <- left_join(shp, count, by = c("sa2_name16" = "sa2_name16")) %>%
- mutate(crashes_sq_km = round(crashes / areasqkm16, 2),
-        decile = ntile(crashes_sq_km, 10),
-        color = ifelse(decile == 1, "blue", ifelse(decile == 10, "red", "black")),
-        bins = ifelse(decile == 1 ,"Area in bottom 10% for accidents", ifelse(decile == 10, "Area in top 10% for accidents", NA)))
- filter(!is.na(crashes)) %>%
-glimpse(act_crashes_sa2)
-
+# Join  count with sa2 in ACT, calc crash density
+act_crashes_sa2 <- left_join(sa2_shp, count, 
+  by = c("sa2_name16" = "sa2_name16"))  %>%
+  mutate(crashes_sq_km = round(crashes / areasqkm16, 2),
+         decile = ntile(crashes_sq_km, 10),
+         color = ifelse(decile == 1, "blue",
+                 ifelse(decile == 10, "red",
+                "black")),
+         bins = ifelse(decile == 1 ,"Area in bottom 10% for accidents", 
+                ifelse(decile == 10, "Area in top 10% for accidents", NA))
+         ) %>%
+ filter(!is.na(crashes))
